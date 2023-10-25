3b7df060f7739640fb315b1719d49d96d808db05
Белова Ольга <OBelova2003@yandex.ru>
2023-10-25 16:56:04 +0300

    main users model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+LENGTH = 150
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=254, unique=True, blank=False,
+                              null=False, verbose_name="Почта")
+
+    username = models.CharField(max_length=LENGTH, unique=True, blank=False,
+                                null=False, verbose_name='Никнейм')
+
+    first_name = models.CharField(max_length=LENGTH, blank=False,
+                                  null=False, verbose_name="Имя")
+
+    last_name = models.CharField(max_length=LENGTH, blank=False,
+                                 null=False, verbose_name="Фамилия")
+
+    REQUIRED_FIELDS = ['email', 'username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
