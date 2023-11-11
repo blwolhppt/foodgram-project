568cc6c4abe80f94c796224c1e8dcdde33b9d75a
Белова Ольга <OBelova2003@yandex.ru>
2023-11-11 20:09:17 +0300

    make mapping.delete

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,10 +3,10 @@ from recipes.models import Recipe, Tag
 
 
 class RecipeFilter(django_filters.FilterSet):
-    author = django_filters.NumberFilter(field_name="author__id")
     tags = django_filters.ModelMultipleChoiceFilter(field_name='tags__slug',
                                                     to_field_name='slug',
                                                     queryset=Tag.objects.all())
+    is_favorited = django_filters.BooleanFilter(method='get_is_favorited')
 
     class Meta:
         model = Recipe
--- backend/api/views.py
+++ backend/api/views.py
@@ -56,49 +56,45 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
         return serializers.NewRecipeSerializer
 
-    @action(
-        detail=True,
-        methods=('POST', 'DELETE',),
-        permission_classes=(IsAuthenticated,)
-    )
-    def favorite(self, request, **kwargs):
-        if request.method == 'POST':
-            return self.add_favorite(request, kwargs.get('pk'))
-        if request.method == 'DELETE':
-            return self.delete_favorite(request, kwargs.get('pk'))
+    @action(detail=True,
+            methods=('POST',),
+            permission_classes=[IsAuthenticated])
+    def favorite(self, request, pk=None):
+        return self.add_favorite(request, pk)
 
-    def add_favorite(self, request, pk):
-        try:
-            recipe = Recipe.objects.get(id=pk)
-        except Recipe.DoesNotExist:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        if FavoriteRecipe.objects.filter(user=request.user,
-                                         recipe=recipe).exists():
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        serializer = serializers.FavoriteRecipeSerializer(
-            FavoriteRecipe.objects.create(user=request.user, recipe=recipe),
-            context={'request': request})
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def delete_favorite(self, request, pk):
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk=None):
         recipe = get_object_or_404(Recipe, id=pk)
         if get_object_or_404(FavoriteRecipe, user=request.user,
                              recipe=recipe).delete():
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+    def add_favorite(self, request, pk):
+        try:
+            recipe = Recipe.objects.get(id=pk)
+        except Recipe.DoesNotExist:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if FavoriteRecipe.objects.filter(user=request.user,
+                                         recipe=recipe).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = serializers.FavoriteRecipeSerializer(
+            FavoriteRecipe.objects.create(user=request.user, recipe=recipe),
+            context={'request': request}
+        )
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     @action(
         detail=True,
-        methods=('POST', 'DELETE',),
+        methods=('POST',),
         permission_classes=(IsAuthenticated,)
     )
-    def shopping_cart(self, request, **kwargs):
-        if request.method == 'POST':
-            return self.add_shopping_cart(request, kwargs.get('pk'))
-        if request.method == 'DELETE':
-            return self.delete_shopping_cart(request, kwargs.get('pk'))
+    def shopping_cart(self, request, pk=None):
+        return self.add_shopping_cart(request, pk)
 
-    def add_shopping_cart(self, request, pk):
+    def add_shopping_cart(self, request, pk=None):
         try:
             recipe = Recipe.objects.get(id=pk)
         except Recipe.DoesNotExist:
@@ -112,7 +108,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
             context={'request': request})
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def delete_shopping_cart(self, request, pk):
+    @shopping_cart.mapping.delete
+    def delete_shopping_cart(self, request, pk=None):
         recipe = get_object_or_404(Recipe, id=pk)
         if get_object_or_404(ListProducts, user=request.user,
                              recipe=recipe).delete():
@@ -122,16 +119,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(
         detail=False,
         methods=('GET',),
-        permission_classes=(IsAuthenticated, ),
+        permission_classes=(IsAuthenticated,),
     )
     def download_shopping_cart(self, request):
-        ingredients = Recipe.objects.filter(
+        ingredients = (Recipe.objects.filter(
             listproducts__user=request.user).values(
             'ingredients__name', 'ingredients__measurement_unit')
+                       .order_by("ingredient__name"))
 
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = \
-            'attachment; filename="shopping_cart.pdf"'
+        response['Content-Disposition'] = ('attachment; filename="'
+                                           'shopping_cart.pdf"')
 
         pdf = canvas.Canvas(response)
 
@@ -140,8 +138,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
         y_position = 780
 
         for ing in ingredients:
-            ingredient_text = f'{ing["ingredients__name"]} ' \
-                              f'({ing["ingredients__measurement_unit" ]})'
+            ingredient_text = (f'{ing["ingredients__name"]} '
+                               f'({ing["ingredients__measurement_unit"]})')
             pdf.drawString(100, y_position, ingredient_text.encode(
                 'utf-8').decode('latin-1'))
             y_position -= 20
