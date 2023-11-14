70d48aa286369f5949f5dfe821bdd67e70704481
Белова Ольга <OBelova2003@yandex.ru>
2023-11-14 14:37:11 +0300

    valid_recipe

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', default=' ')
 
-DEBUG = 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, '
                                                    'localhost').split(',')
